101d3865859acaf9c413d68f20aeeced18d7d339
Freyja <freyamarika@gmail.com>
2024-11-23 20:31:57 +1100

    vertex and fragment shaders

---
+++ tools/batch/shader.bat
@@ -0,0 +1,3 @@
+"C:\VulkanSDK\1.3.296.0\Bin\glslc.exe" "./../../src/shaders/shader.vert" -o "./../../data/shaders/vert.spv"
+"C:\VulkanSDK\1.3.296.0\Bin\glslc.exe" "./../../src/shaders/shader.frag" -o "./../../data/shaders/frag.spv"
+pause
